3195983231bc41228756662a76e30ff7b1bcdf07
Jaime Alexis García Hinojosa <alekzgh@gmail.com>
2017-08-25 12:21:34 -0500

    Revert "Prueba"
    This reverts commit ec7924fefa83267a034de23c2cee9a73fe10d43f.

--- src/vista/FramePDF.java
+++ src/vista/FramePDF.java
@@ -78,6 +78,7 @@ public class FramePDF extends javax.swing.JFrame {
         pnlBackground = new javax.swing.JPanel();
         pnlBotonesArx = new javax.swing.JPanel();
         btnBuscarFactura = new javax.swing.JButton();
+        cmbCompania = new javax.swing.JComboBox<>();
         btnGuardar = new javax.swing.JButton();
         pnlIndicadorArx = new javax.swing.JPanel();
         jSeparator1 = new javax.swing.JSeparator();
@@ -115,6 +116,8 @@ public class FramePDF extends javax.swing.JFrame {
             }
         });
 
+        cmbCompania.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar Cia" }));
+
         btnGuardar.setText("Guardar");
         btnGuardar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -129,7 +132,9 @@ public class FramePDF extends javax.swing.JFrame {
             .addGroup(pnlBotonesArxLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(btnBuscarFactura, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 338, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 89, Short.MAX_VALUE)
+                .addComponent(cmbCompania, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 89, Short.MAX_VALUE)
                 .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -138,6 +143,7 @@ public class FramePDF extends javax.swing.JFrame {
             .addGroup(pnlBotonesArxLayout.createSequentialGroup()
                 .addGroup(pnlBotonesArxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnBuscarFactura, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cmbCompania, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(0, 8, Short.MAX_VALUE))
         );
@@ -443,6 +449,7 @@ public class FramePDF extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBuscarFactura;
     private javax.swing.JButton btnGuardar;
+    private javax.swing.JComboBox<String> cmbCompania;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel lblIndicadorArx;
     private javax.swing.JMenuItem mniEdicionArchivo;
